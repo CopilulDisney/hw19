8e9072442bccfe3e9015684379b5cb4e3af6bb37
Vasile <vasile.amethyst69@gmail.com>
2023-10-11 17:35:44 +0300

    first

---
+++ index.js
@@ -0,0 +1,80 @@
+const arr = [1,2,3,4,5]
+const arr1 = [6,7,8,9,10]
+
+// for (let i = 10; i > 0; i--) { //Exercitiu 1
+//     console.log(i)
+// }
+
+// let i = 0
+// while (i < 11){
+//     console.log(i)
+//     i++
+// }
+
+// let i1 = 10
+// do {
+//     console.log(i1++)
+// } while (i1 > 11)
+
+// for (let i = 5; i > 0; i--){ //Exercitiu 2
+//     console.log(i)
+// }
+
+// console.log(arr.map((item) =>{ //Exercitiu 3
+//     console.log('element', item)
+// }))
+
+// let str = 'string' //Exercitiu 4
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i])
+// }
+
+// console.log(arr.map((array) =>{ //Exercitiu 5
+//     console.log('arr', array)
+// }))
+
+// const arr1 = arr.filter((index) =>{
+//     return index >1
+// })
+// console.log(arr1)
+
+// const idx = arr.indexOf(3)
+// console.log('idx', idx)
+
+// const idx1 = arr.findIndex((item) => {
+//     return item > 2
+// })
+
+// console.log(idx1)
+
+// const every = arr.every((item) => {
+//     return item < 0
+// })
+
+// console.log('every', every)
+
+// const some = arr.some((item) =>{
+//     return item >= 3
+// })
+
+// console.log('some',some)
+
+// console.log('flat', arr.flat())
+
+// arr.forEach((index) => {
+//     console.log('index:', index)
+// })
+
+// const found = arr.find((item) => {
+//     return item > 1
+// })
+
+// console.log('found',found)
+
+// const arr2 = arr.concat(arr1)
+// console.log('arr2', arr2)
+
+// arr3 = [...arr, ...arr1];
+// console.log(arr3)
+
+console.log('includes:', arr.includes(2))
